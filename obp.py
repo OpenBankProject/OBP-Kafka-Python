f1d82ee66f5ab2572bd47d347d7b7b8fac6ebb55
Petar Bozin <petar.bozin@md-softwaredesign.de>
2015-11-16 22:46:38 +0100

    Added getBankAccount

--- obp.py
+++ obp.py
@@ -65,36 +65,94 @@ def getBanks(args):
 # returns string
 #
 def getTransactions(args):
-
   # hardcoded values for transaction1 
-  bank                      = "123"
-  account                   = "12345"
-  transactionId             = "1"
-  transactionType           = "someType"
-  amount                    = "10,000.00"
-  newAccountBalance         = "10,000.00"
-  currency                  = "Dollar"
-  tStartDate                = "Fri Nov 13 17:57:02 CET 2015"
-  tFinishDate               = "Fri Nov 13 17:57:04 CET 2015"
-  description               = "Test transaction"
-  counterpartyAccountHolder = "holder"
-  counterpartyAccountNumber = "222"
-
+  bankId          = "123"
+  accountId       = "12345"
+  transactionId   = "1"
+  transactionType = "current"
+  amount          = "10,000.00"
+  currency        = "EUR"
+  description     = "Test transaction"
+  startDate       = "Fri Nov 13 17:57:02 CET 2015"
+  finishDate      = "Fri Nov 13 17:57:04 CET 2015"
+  balance         = "10,000.00"
+  otherBankId     = "321"
+  otherAccountId  = "54321"
   # assemble the return string
   r  = '{'
-  r += 'bank:"'                      + bank                      + '",' 
-  r += 'account:"'                   + account                   + '",' 
-  r += 'transactionId:"'             + transactionId             + '",' 
-  r += 'transactionType:"'           + transactionType           + '",' 
-  r += 'amount:"'                    + amount                    + '",' 
-  r += 'newAccountBalance:"'         + newAccountBalance         + '",' 
-  r += 'currency:"'                  + currency                  + '",' 
-  r += 'tFinishDate:"'               + tFinishDate               + '",' 
-  r += 'tStartDate:"'                + tStartDate                + '",' 
-  r += 'description:"'               + description               + '",' 
-  r += 'counterpartyAccountHolder:"' + counterpartyAccountHolder + '",'
-  r += 'counterpartyAccountNumber:"' + counterpartyAccountNumber + '"'
+  r += 'bankId:"'          + bankId          + '",' 
+  r += 'accountId:"'       + accountId       + '",' 
+  r += 'transactionId:"'   + transactionId   + '",' 
+  r += 'transactionType:"' + transactionType + '",' 
+  r += 'amount:"'          + amount          + '",' 
+  r += 'currency:"'        + currency        + '",' 
+  r += 'description:"'     + description     + '",' 
+  r += 'startDate:"'       + startDate       + '",' 
+  r += 'finishDate:"'      + finishDate      + '",' 
+  r += 'balance:"'         + balance         + '",' 
+  r += 'otherBankId:"'     + otherBankId     + '",'
+  r += 'otherAccountId:"'  + otherAccountId  + '"'
   r += '}' 
   # return result
   return r
 
+
+# getBankAccount returns bank account data 
+# accepts arguments: bankId and accountId
+# returns string
+#
+def getBankAccount(args):
+  # get argument
+  bankId = args['bankId']
+  accountId = args['accountId']
+  if not bankId or not accountId:
+    # return error if empty
+    return '{error:"no argument given"}'
+  if bankId == "123" and accountId == "12345":
+    # hardcoded values for bankAccount1 
+    accountId       = "12345"
+    accountType     = "savings"
+    balance         = "20,000.00"
+    currency        = "EUR"
+    name            = "Test transaction"
+    label           = "test_transaction"
+    swift_bic       = "SWIFT_BIC"
+    iban            = "IBAN12345"
+    number          = "123451234512345"
+    bankId          = "123"
+    lastUpdate      = "Fri Nov 13 17:57:04 CET 2015"
+    accountHolder   = "ACCOUNT_HOLDER"  #deprecated?
+  elif bankId == "321" and accountId == "54321":
+    # hardcoded values for bankAccount2
+    accountId       = "54321"
+    accountType     = "savings"
+    balance         = "15,000.00"
+    currency        = "EUR"
+    name            = "Transaction testing"
+    label           = "transaction_testing"
+    swift_bic       = "BIC_SWIFT"
+    iban            = "IBAN54321"
+    number          = "543215432154321"
+    bankId          = "321"
+    lastUpdate      = "Sat Nov 14 11:37:14 CET 2015"
+    accountHolder   = "HOLDER_OF_THE_ACCOUNT"  #deprecated?
+  else:
+    # just two bank accounts in this demo
+    return '{}'
+  # assemble the return string
+  r  = '{'
+  r += 'accountId:"'       + accountId       + '",' 
+  r += 'accountType:"'     + accountType     + '",' 
+  r += 'balance:"'         + balance         + '",' 
+  r += 'currency:"'        + currency        + '",' 
+  r += 'name:"'            + name            + '",' 
+  r += 'label:"'           + label           + '",' 
+  r += 'swift_bic:"'       + swift_bic       + '",' 
+  r += 'iban:"'            + iban            + '",' 
+  r += 'number:"'          + number          + '",'
+  r += 'bankId:"'          + bankId          + '",' 
+  r += 'lastUpdate:"'      + lastUpdate      + '",'
+  r += 'accountHolder:"'   + accountHolder   + '"' #deprecated?
+  r += '}' 
+  # return result
+  return r
