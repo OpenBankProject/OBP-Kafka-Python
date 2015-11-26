a2afd70ac834dec7bc8a715e78f7fcc7a0638e21
root <root@magicbox.devel.acheron-services.net>
2015-11-26 10:35:14 +0100

    AddTransactions

--- obp.py
+++ obp.py
@@ -21,34 +21,21 @@ def getBank(args):
   bankId = args['bankId']
   if not bankId:
     # return error if empty
-    return '{error:"no argument given"}'
-  if bankId   == "123":
-    # hardcoded values for bank1 
-    permalink     = "123" 
-    fullBankName  = "Test Bank"
-    shortBankName = "tbank"
-    logoURL       = "http://tbank.tb/images/logo.png"
-    websiteURL    = "http://tbank.tb"
-  elif bankId == "321":
-    # hardcoded values for bank2
-    permalink     = "321" 
-    fullBankName  = "Another Bank"
-    shortBankName = "abank"
-    logoURL       = "http://abank.ab/images/logo.png"
-    websiteURL    = "http://abank.ab"
-  else:
-    # just two banks in this demo
-    return '{}'
-  # assemble the return string
-  r  = '{'
-  r += '"permalink":"'     + permalink     + '",' 
-  r += '"fullBankName":"'  + fullBankName  + '",' 
-  r += '"shortBankName":"' + shortBankName + '",' 
-  r += '"logoURL":"'       + logoURL       + '",' 
-  r += '"websiteURL":"'    + websiteURL    + '"'
-  r += '}' 
-  # return result
-  return r
+    return '{"error":"no argument given"}'
+  for b in banks:
+    if bankId == b["id"]:
+      # assemble the return string
+      r  = '{'
+      r += '"bankId":"'        + b["id"]         + '",' 
+      r += '"shortBankName":"' + b["short_name"] + '",' 
+      r += '"fullBankName":"'  + b["full_name"]  + '",' 
+      r += '"logoURL":"'       + b["logo"]       + '",' 
+      r += '"websiteURL":"'    + b["website"]    + '"'
+      r += '}' 
+      # return result
+      return r
+  # return empty if not found 
+  return '{}'
 
 # getBanks returns list of all banks
 # accepts no arguments
@@ -56,18 +43,20 @@ def getBank(args):
 #
 def getBanks(args):
   global banks 
-  # list of bankIds to return
-  bankList  = ["123", "321"] 
-  # opening bracket
   r  =  '{' 
-  # loop over all bankIds
-  for bank in bankList:
-    # get bank data calling getBank with bankId as argument
-    r += getBank({"bankId":bank})
-    # add comma after each entry
-    r += '",'
+  for b in banks:
+    # assemble the return string
+    r  = '{'
+    r += '"bankId":"'        + b["id"]         + '",' 
+    r += '"shortBankName":"' + b["short_name"] + '",' 
+    r += '"fullBankName":"'  + b["full_name"]  + '",' 
+    r += '"logoURL":"'       + b["logo"]       + '",' 
+    r += '"websiteURL":"'    + b["website"]    + '"'
+    r += '}' 
+    r += ','
   # remove trailing comma from result string
-  r  = r[:-1]  
+  if r.endswith(","):
+    r  = r[:-1]  
   # closing bracket
   r  +=  '}' 
   # return result
@@ -102,6 +91,7 @@ def getTransaction(args):
       r += '}' 
       # return result
       return r
+  # return empty if not found 
   return '{}'
 
 # getTransactions returns list of transactions depending on queryParams
@@ -113,23 +103,29 @@ def getTransactions(args):
   # get arguments
   bankId  = args['bankId'] 
   accountId = args['accountId']
-  # mock search results according to queryParams
-  if (bankId == "obp-bank-x-gh" and accountID == "test-x"):
-    transactionIds = ['1','2']
-  elif (bankId == "obp-bank-y-gh" and accountID == "test-y"):
-    transactionIds = ['3']
-  else:
-    return '{}'
-  # opening bracket
+  queryParams = args['queryParams']
   r  =  '{' 
-  # loop over all bankIds
-  for transactionId in transactionIds:
-    # get bank data calling getBank with bankId as argument
-    r += getTransaction({"bankId":bankId,"accountId":accountId,"transactionId":transactionId})
-    # add comma after each entry
-    r += '",'
+  for t in transactions:
+    if bankId == t["this_account"]["bank"] and accountId == t["this_account"]["id"]:
+      # assemble the return string
+      r += '{'
+      r += '"bankId":"'          + t["this_account"]["bank"]   + '",' 
+      r += '"accountId":"'       + t["this_account"]["id"]     + '",' 
+      r += '"transactionId":"'   + t["id"]                     + '",' 
+      r += '"transactionType":"' + t["details"]["type"]        + '",' 
+      r += '"amount":"'          + t["details"]["value"]       + '",' 
+      r += '"currency":"'        + "GBP"                       + '",' 
+      r += '"description":"'     + t["details"]["description"] + '",' 
+      r += '"startDate":"'       + t["details"]["posted"]      + '",' 
+      r += '"finishDate":"'      + t["details"]["completed"]   + '",' 
+      r += '"balance":"'         + t["details"]["new_balance"] + '",' 
+      r += '"otherBankId":"'     + "obp-bank-x-g"              + '",'
+      r += '"otherAccountId":"'  + "2330135d-fca8-4268-838d-833074985209"  + '"'
+      r += '}' 
+      r += ','
   # remove trailing comma from result string
-  r  = r[:-1]  
+  if r.endswith(","):
+    r  = r[:-1]  
   # closing bracket
   r  +=  '}' 
   # return result
@@ -140,58 +136,31 @@ def getTransactions(args):
 # returns string
 #
 def getBankAccount(args):
-  global data
+  global accounts 
   # get arguments
   bankId = args['bankId']
   accountId = args['accountId']
   if not bankId or not accountId:
     # return error if empty
-    return '{error:"no argument given"}'
-  if bankId == "123" and accountId == "12345":
-    # hardcoded values for bankAccount1 
-    accountId       = "12345"
-    accountType     = "savings"
-    balance         = "20000"
-    currency        = "EUR"
-    name            = "Test transaction"
-    label           = "test_transaction"
-    swift_bic       = "SWIFT_BIC"
-    iban            = "IBAN12345"
-    number          = "123451234512345"
-    bankId          = "123"
-    lastUpdate      = "Fri Nov 13 17:57:04 CET 2015"
-    accountHolder   = "ACCOUNT_HOLDER"  #deprecated?
-  elif bankId == "321" and accountId == "54321":
-    # hardcoded values for bankAccount2
-    accountId       = "54321"
-    accountType     = "savings"
-    balance         = "15000"
-    currency        = "EUR"
-    name            = "Transaction testing"
-    label           = "transaction_testing"
-    swift_bic       = "BIC_SWIFT"
-    iban            = "IBAN54321"
-    number          = "543215432154321"
-    bankId          = "321"
-    lastUpdate      = "Sat Nov 14 11:37:14 CET 2015"
-    accountHolder   = "HOLDER_OF_THE_ACCOUNT"  #deprecated?
-  else:
-    # just two bank accounts in this demo
-    return '{}'
-  # assemble the return string
-  r  = '{'
-  r += '"accountId":"'       + accountId       + '",' 
-  r += '"accountType":"'     + accountType     + '",' 
-  r += '"balance":"'         + balance         + '",' 
-  r += '"currency":"'        + currency        + '",' 
-  r += '"name":"'            + name            + '",' 
-  r += '"label":"'           + label           + '",' 
-  r += '"swift_bic":"'       + swift_bic       + '",' 
-  r += '"iban":"'            + iban            + '",' 
-  r += '"number":"'          + number          + '",'
-  r += '"bankId":"'          + bankId          + '",' 
-  r += '"lastUpdate":"'      + lastUpdate      + '",'
-  r += '"accountHolder":"'   + accountHolder   + '"' #deprecated?
-  r += '}' 
-  # return result
-  return r
+    return '{"error":"no argument given"}'
+  for a in accounts:
+    if bankId == a["bank"] and accountId == a["id"]:
+      # assemble the return string
+      r  = '{'
+      r += '"accountId":"'       + a["id"]                    + '",' 
+      r += '"accountType":"'     + a["type"]                  + '",' 
+      r += '"balance":"'         + a["balance"]["amount"]     + '",' 
+      r += '"currency":"'        + a["balance"]["currency"]   + '",' 
+      r += '"name":"'            + a["owners"]                + '",' 
+      r += '"label":"'           + a["label"]                 + '",' 
+      r += '"swift_bic":"'       + "SWIFT_BIC"                + '",' 
+      r += '"iban":"'            + a["IBAN"]                  + '",' 
+      r += '"number":"'          + a["number"]                + '",'
+      r += '"bankId":"'          + a["bank"]                  + '",' 
+      r += '"lastUpdate":"'      + "2015-07-01T00:00:00.000Z" + '",'
+      r += '"accountHolder":"'   + a["owners"]                + '"' #deprecated?
+      r += '}' 
+      # return result
+      return r
+  # return empty if not found 
+  return '{}'
